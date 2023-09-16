be42e53607e2a5c9be90328e89591f35c478a2af
Eliran Shemesh <piro27@gmail.com>
2023-09-16 12:10:04 +0300

    Add current time support

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { snakeCase, noop } from "lodash";
 import { SubmitButton } from "./SubmitButton";
 import { getTime } from "../../utils/time";
+import { DateTime } from "luxon";
 
 export const Input = ({
   name,
@@ -12,7 +13,8 @@ export const Input = ({
   min,
   max,
   refetch = noop,
-  submitSameValues = false
+  submitSameValues = false,
+  isTimeInput = false,
 }) => {
   const inputValue = useMemo(() => {
     if (value) return value;
@@ -20,6 +22,10 @@ export const Input = ({
     if (values && values.length > 0) {
       const lastValue = values.at(-1);
       return Object.values(lastValue)[0];
+    }
+    
+    if (isTimeInput) {
+      return DateTime.now().toFormat("HH:mm");
     }
     
     return "";
