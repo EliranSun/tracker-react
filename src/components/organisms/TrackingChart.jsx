d61cbf2c10a581bc45a8127447772cc54ca8831b
RyanMcklain <piro27@gmail.com>
2023-09-16 14:17:06 +0300

    Refactor datasets to be in hook

--- src/components/organisms/TrackingChart.jsx
+++ src/components/organisms/TrackingChart.jsx
@@ -1,19 +1,19 @@
 import {
+  BarController,
+  BarElement,
+  CategoryScale,
   Chart as ChartJS,
-  LineElement,
-  Tooltip,
   Legend,
   LinearScale,
-  CategoryScale,
+  LineElement,
   PointElement,
-  BarElement,
   RadialLinearScale,
   ScatterController,
-  BarController,
+  Tooltip,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 import { useChartData } from "../../hooks/useChartData";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { getLocaleDate } from "../../utils/date";
 
 ChartJS.register(
@@ -29,126 +29,6 @@ ChartJS.register(
   BarController
 );
 
-const getData = ({ labels, data, isDayView }) => {
-  if (!labels || !data) {
-    return {
-      labels: [],
-      datasets: [],
-    };
-  }
-
-  const energyData = {
-    type: "line",
-    label: "Energy levels",
-    data: data.energy,
-    tension: 0,
-    fill: false,
-    borderWidth: 3,
-    yAxisID: "y",
-    borderColor: "rgb(235,205,53)",
-    backgroundColor: "rgba(235,220,53,0.5)",
-  };
-  const coffeeData = {
-    type: "scatter",
-    label: "Coffee",
-    data: data.coffee,
-    borderWidth: 3,
-    yAxisID: "y1",
-    borderColor: "rgb(225,203,189)",
-    backgroundColor: "rgba(110,81,53,0.5)",
-  };
-  const productivityData = {
-    type: "bar",
-    stack: true,
-    label: "Productivity",
-    data: data.productivity,
-    borderWidth: 3,
-    yAxisID: "y2",
-    borderColor: "rgb(235,53,53)",
-    backgroundColor: "rgba(122,40,40,0.5)",
-  };
-  const creativeData = {
-    type: "bar",
-    stack: true,
-    label: "Creativity",
-    data: data.creative,
-    borderWidth: 3,
-    yAxisID: "y2",
-    borderColor: "rgb(196,53,235)",
-    backgroundColor: "rgba(183,53,235,0.5)",
-  };
-  const socialData = {
-    type: "bar",
-    stack: true,
-    label: "Social",
-    data: data.social,
-    borderWidth: 3,
-    yAxisID: "y2",
-    borderColor: "rgb(53, 162, 235)",
-    backgroundColor: "rgba(53, 162, 235, 0.5)",
-  };
-
-  const datasets = [];
-
-  data.energy && datasets.push(energyData);
-  data.coffee && datasets.push(coffeeData);
-  data.productivity && datasets.push(productivityData);
-  data.creative && datasets.push(creativeData);
-  data.social && datasets.push(socialData);
-
-  return {
-    labels,
-    datasets: [
-      ...datasets,
-      {
-        type: "scatter",
-        label: "Woke mid night",
-        data: data.wokeUpMidNight,
-        borderColor: "blue",
-        yAxisID: "y1",
-        stepped: true,
-      },
-      {
-        type: "scatter",
-        label: "Snooze",
-        yAxisID: "y1",
-        data: data.snooze,
-        borderColor: "yellow",
-        stepped: true,
-      },
-      {
-        type: "bar",
-        label: "Sleep",
-        data: data.sleep,
-        borderWidth: 3,
-        yAxisID: "y1",
-        indexAxis: isDayView ? 'y' : 'x',
-        borderColor: "rgb(66,161,143)",
-        backgroundColor: "rgb(79,180,161)",
-      },
-      {
-        type: "bar",
-        label: "Nap",
-        data: data.nap,
-        borderWidth: 3,
-        yAxisID: "y1",
-        borderColor: "green",
-        backgroundColor: "green",
-      },
-      {
-        type: "bar",
-        stack: true,
-        label: "YouTube",
-        data: data.youtube,
-        borderWidth: 3,
-        yAxisID: "y2",
-        borderColor: "red",
-        backgroundColor: "red",
-      },
-    ],
-  };
-};
-
 const TwentyFourHoursFifteenMinutesIntervals = Array.from(
   Array(24 * 4).keys()
 ).map((i) => {
@@ -157,77 +37,73 @@ const TwentyFourHoursFifteenMinutesIntervals = Array.from(
   return `${hour}:${minute === 0 ? "00" : minute}`;
 });
 
-const options = {
-  interaction: {
-    mode: "index",
-    intersect: false,
-  },
-  cubicInterpolationMode: "monotone",
-  scales: {
-    y: {
-      stacked: false,
-    },
-    y1: {
-      adapter: "luxon",
-      type: "category",
-      labels: TwentyFourHoursFifteenMinutesIntervals,
-      stack: true,
-      time: {
-        unit: "hour",
-      },
-      display: true,
-      position: "right",
-      grid: {
-        drawOnChartArea: false, // only want the grid lines for one axis to show up
-      },
-    },
-    y2: {
-      beginAtZero: false,
-    },
-  },
-  plugins: {
-    tooltip: {
-      callbacks: {
-        label: function (context) {
-          if (context.dataset.label.toLowerCase() !== "sleep") {
-            return `${context.dataset.label}: ${context.formattedValue}`;
-          }
-
-          let label = "Sleep:";
-          const data = context.raw?.y;
-          if (data.length === 2) {
-            label += `${data[0]} - ${data[1]}`;
-          }
-
-          if (data.length === 4) {
-            label += `${data[2]} - ${data[1]}`;
-          }
-
-          return label;
-        },
-      },
-    },
-  },
-};
-
 export const TrackingChart = ({ date }) => {
   const [isDayView, setIsDayView] = useState(false);
-  const { labels, data } = useChartData({
+  const data = useChartData({
     date: isDayView && getLocaleDate(date ? new Date(date) : undefined),
   });
-  const formattedData = useMemo(() => {
-    return getData({
-      labels,
-      data,
-      isDayView
-    });
-  }, [labels, data, isDayView]);
 
   return (
     <div style={{ height: "50vh" }}>
       <button onClick={() => setIsDayView(true)}>DAY</button>
       <button onClick={() => setIsDayView(false)}>WEEK</button>
-      <Line options={options} data={formattedData} />
+      <Line
+        data={data}
+        options={{
+          interaction: {
+            mode: "index",
+            intersect: false,
+          },
+          cubicInterpolationMode: "monotone",
+          scales: {
+            y: {
+              stacked: false,
+            },
+            y1: {
+              adapter: "luxon",
+              type: "category",
+              labels: TwentyFourHoursFifteenMinutesIntervals,
+              stack: true,
+              time: {
+                unit: "hour",
+              },
+              display: true,
+              position: "right",
+              grid: {
+                drawOnChartArea: false, // only want the grid lines for one axis to show up
+              },
+            },
+            y2: {
+              beginAtZero: false,
+            },
+          },
+          legend: {
+            hidden: true
+          },
+          plugins: {
+            tooltip: {
+              callbacks: {
+                label: function (context) {
+                  if (context.dataset.label.toLowerCase() !== "sleep") {
+                    return `${context.dataset.label}: ${context.formattedValue}`;
+                  }
+
+                  let label = "Sleep:";
+                  const data = context.raw?.y;
+                  if (data.length === 2) {
+                    label += `${data[0]} - ${data[1]}`;
+                  }
+
+                  if (data.length === 4) {
+                    label += `${data[2]} - ${data[1]}`;
+                  }
+
+                  return label;
+                },
+              },
+            },
+          },
+        }}/>
     </div>
   );
 };
--- src/hooks/useChartData.js
+++ src/hooks/useChartData.js
@@ -91,7 +91,10 @@ const extractSleepHoursData = (data = [], isDayView = false) => {
 export const useChartData = ({ date }) => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({
+    labels: [],
+    datasets: [],
+  });
 
   useEffect(() => {
     setIsLoading(true);
@@ -102,11 +105,11 @@ export const useChartData = ({ date }) => {
           return;
         }
 
-        const foo = {};
+        const formattedData = {};
         Object.keys(new Row()).forEach((key) => {
-          foo[key] = [];
+          formattedData[key] = [];
         });
-        foo.sleep = [];
+        formattedData.sleep = [];
 
         data
           .map((item) => ({ ...item, timestamp: new Date(item.date) }))
@@ -115,25 +118,20 @@ export const useChartData = ({ date }) => {
             const rowDate = row.date;
             const x = date ? "12:00" : row.date;
 
-            foo.creative.push({ y: row.creative, x });
-            foo.social.push({ y: row.social, x });
-            foo.youtube.push({ y: row.youtube, x });
-            foo.productivity.push({ y: row.productivity, x });
-
-            foo.sleep.push(...extractSleepHoursData(row, Boolean(date)));
-            foo.coffee.push(
-              ...extractTimeBasedData(row.coffee, rowDate, Boolean(date))
-            );
-            foo.energy.push(
-              ...extractValueBasedData(row.energy, x, Boolean(date))
-            );
+            formattedData.creative.push({ y: row.creative, x });
+            formattedData.social.push({ y: row.social, x });
+            formattedData.youtube.push({ y: row.youtube, x });
+            formattedData.productivity.push({ y: row.productivity, x });
+            formattedData.sleep.push(...extractSleepHoursData(row, Boolean(date)));
+            formattedData.coffee.push(...extractTimeBasedData(row.coffee, rowDate, Boolean(date)));
+            formattedData.energy.push(...extractValueBasedData(row.energy, x, Boolean(date)));
 
             const napOverTime = DateTime.fromObject({ hour: 15, minute: 0 })
               .plus({ minutes: row.nap })
               .toFormat("HH:mm");
-            row.wokeUpMidNight && foo.wokeUpMidNight.push({ y: "4:00", x });
-            row.snooze && foo.snooze.push({ y: "7:00", x });
-            row.nap && foo.nap.push({ y: ["15:00", napOverTime], x });
+            row.wokeUpMidNight && formattedData.wokeUpMidNight.push({ y: "4:00", x });
+            row.snooze && formattedData.snooze.push({ y: "7:00", x });
+            row.nap && formattedData.nap.push({ y: ["15:00", napOverTime], x });
           });
 
         const labels = Array.from({ length: date ? 12 * 4 : 7 })
@@ -155,10 +153,116 @@ export const useChartData = ({ date }) => {
           })
           .reverse();
 
-        const labels2 = foo.energy.map((item) => item.x);
+        const labels2 = formattedData.energy.map((item) => item.x);
+        const isDayView = Boolean(date);
+
         setData({
-          data: foo,
-          labels: labels,
+          labels,
+          datasets: [
+            {
+              type: "line",
+              label: "Energy levels",
+              data: formattedData.energy,
+              tension: 0,
+              fill: false,
+              borderWidth: 3,
+              yAxisID: "y",
+              borderColor: "rgb(235,205,53)",
+              backgroundColor: "rgba(235,220,53,0.5)",
+            },
+            {
+              type: "scatter",
+              label: "Coffee",
+              data: formattedData.coffee,
+              borderWidth: 3,
+              yAxisID: "y1",
+              borderColor: "rgb(225,203,189)",
+              backgroundColor: "rgba(110,81,53,0.5)",
+              hidden: true,
+            },
+            {
+              type: "bar",
+              stack: true,
+              label: "Productivity",
+              data: formattedData.productivity,
+              borderWidth: 3,
+              yAxisID: "y2",
+              borderColor: "rgb(235,53,53)",
+              backgroundColor: "rgba(122,40,40,0.5)",
+              hidden: true,
+            },
+            {
+              type: "bar",
+              stack: true,
+              label: "Creativity",
+              data: formattedData.creative,
+              borderWidth: 3,
+              yAxisID: "y2",
+              borderColor: "rgb(196,53,235)",
+              backgroundColor: "rgba(183,53,235,0.5)",
+              hidden: true,
+            },
+            {
+              type: "bar",
+              stack: true,
+              label: "Social",
+              data: formattedData.social,
+              borderWidth: 3,
+              yAxisID: "y2",
+              borderColor: "rgb(53, 162, 235)",
+              backgroundColor: "rgba(53, 162, 235, 0.5)",
+              hidden: true,
+            },
+            {
+              type: "scatter",
+              label: "Woke mid night",
+              data: formattedData.wokeUpMidNight,
+              borderColor: "blue",
+              yAxisID: "y1",
+              stepped: true,
+              hidden: true,
+            },
+            {
+              type: "scatter",
+              label: "Snooze",
+              yAxisID: "y1",
+              data: formattedData.snooze,
+              borderColor: "yellow",
+              stepped: true,
+              hidden: true,
+            },
+            {
+              type: "bar",
+              label: "Sleep",
+              data: formattedData.sleep,
+              borderWidth: 3,
+              yAxisID: "y1",
+              indexAxis: isDayView ? 'y' : 'x',
+              borderColor: "rgb(66,161,143)",
+              backgroundColor: "rgb(79,180,161)",
+            },
+            {
+              type: "bar",
+              label: "Nap",
+              data: formattedData.nap,
+              borderWidth: 3,
+              yAxisID: "y1",
+              borderColor: "green",
+              backgroundColor: "green",
+              hidden: true,
+            },
+            {
+              type: "bar",
+              stack: true,
+              label: "YouTube",
+              data: formattedData.youtube,
+              borderWidth: 3,
+              yAxisID: "y2",
+              borderColor: "red",
+              backgroundColor: "red",
+              hidden: true,
+            },
+          ],
         });
       })
       .catch((error) => {
