43f90930177317126c8223f76e0ff9a4a57685ec
Eliran Shemesh <piro27@gmail.com>
2023-09-21 18:45:43 +0300

    Support non array values

--- src/components/atoms/TimeBasedInput.jsx
+++ src/components/atoms/TimeBasedInput.jsx
@@ -1,18 +1,20 @@
 import { getTimeBasedValue } from "../../utils/updateData";
 import { Input } from "./Input";
 import { useCallback, useEffect, useState } from "react";
+import { isArray } from "lodash";
 
 export const TimeBasedInput = ({ name, date, refetch, values }) => {
-  return (
-    <div style={{ display: "flex", justifyContent: "stretch" }}>
-      <Input
-        type="time"
-        name={name}
-        values={values}
-        date={date}
-        refetch={refetch}
-        isTimeInput
-      />
-    </div>
-  );
+  const data = isArray(values) ? values : [values];
+    return (
+        <div style={{ display: "flex", justifyContent: "stretch" }}>
+          <Input
+            type="time"
+            name={name}
+            values={values}
+            date={date}
+            refetch={refetch}
+            isTimeInput
+          />
+        </div>
+      );
 };
