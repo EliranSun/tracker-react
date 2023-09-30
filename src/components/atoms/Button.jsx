12d267b9288d647d7c6dfeebdd2fc0856a6d4755
Eliran Shemesh <piro27@gmail.com>
2023-09-30 17:29:18 +0300

    Add back button min dimensions

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 export const Button = ({ children, className, color, type, ...rest }) => {
   return (
     <button
-      className={classNames("p-4 text-black flex items-center justify-center rounded", className, {
+      className={classNames("min-w-[60px] min-h-[48px] p-4 text-black flex items-center justify-center rounded", className, {
         "bg-white text-black": color === "white",
         "w-20 h-20": type === "box",
       })}
