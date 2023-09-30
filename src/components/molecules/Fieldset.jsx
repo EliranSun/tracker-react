8bfb7805e9078c41fda547f8ddedcbf0d4ccf833
Eliran Shemesh <piro27@gmail.com>
2023-09-30 19:05:00 +0300

    Box border to fix overflow

--- src/components/molecules/Fieldset.jsx
+++ src/components/molecules/Fieldset.jsx
@@ -1,6 +1,6 @@
 export const Fieldset = ({ children, legend }) => {
   return (
-    <fieldset className="border border-white rounded-2xl my-y p-4">
+    <fieldset className="w-full border border-white rounded-2xl box-border my-4 p-4">
       {/*<legend className="text-lg">{legend}</legend>*/}
       {children}
     </fieldset>
