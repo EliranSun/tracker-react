8c417b24cf40568e7b47a8a8b06b6850773ec00b
Eliran Shemesh <piro27@gmail.com>
2023-09-30 17:17:03 +0300

    Remove width/height from button

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 export const Button = ({ children, className, color, type, ...rest }) => {
   return (
     <button
-      className={classNames("min-h-[48px] p-4 min-w-[60px] text-black flex items-center justify-center rounded", className, {
+      className={classNames("p-4 text-black flex items-center justify-center rounded", className, {
         "bg-white text-black": color === "white",
         "w-20 h-20": type === "box",
       })}
