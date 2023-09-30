b951f75f66a4e0bf4e376de7ada7465135d75e77
Eliran Shemesh <piro27@gmail.com>
2023-09-30 17:25:59 +0300

    Update Fieldset padding and border

--- src/components/molecules/Fieldset.jsx
+++ src/components/molecules/Fieldset.jsx
@@ -1,6 +1,6 @@
 export const Fieldset = ({ children, legend }) => {
   return (
-    <fieldset className="">
+    <fieldset className="border border-white rounded-2xl p-4">
       {/*<legend className="text-lg">{legend}</legend>*/}
       {children}
     </fieldset>
